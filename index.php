3459542f5690e73b764c121943258cca0f61e656
Austin Clerkin <aclerkin@intlbancard.com>
2017-12-15 12:12:37 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Hosted Pay Payment HP-p08p5cXj</title>
+  </head>
+  <body>
+  <iframe style="width:100%;height:100%;min-height:700px;border:0 none transparent;"
+          src="https://hosted-pay-public-api.test/payments/HP-<?php echo $_REQUEST['payment']; ?>?XDEBUG_SESSION_START=true"></iframe>
+  </body>
+</html>
